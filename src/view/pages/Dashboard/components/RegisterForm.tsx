b4757b63aa504254dc35eb7f289e5719b445d509
Osvaldo <osvaldobbini@gmail.com>
2024-10-12 21:41:47 -0300

    forms

--- src/view/pages/Dashboard/components/RegisterForm.tsx
+++ src/view/pages/Dashboard/components/RegisterForm.tsx
@@ -1,7 +1,29 @@
+import { X } from "@phosphor-icons/react";
+import { Input } from "../../../components/Input";
+
 export function RegisterForm(): JSX.Element {
+
+  // ${isOpen ? 'block' : 'hidden'}
+
   return (
-    <div className="h-full w-full bg-red-800 absolute z-10">
-      Form
+    <div className="h-full w-full bg-black fixed z-10 bg-opacity-75">
+      <div className="flex justify-end items-end h-full">
+      <form className="bg-white h-4/5 p-4 w-full rounded-t-lg">
+        <header className="flex justify-end">
+          <button>
+            <X size={28}/>
+          </button>
+        </header>
+        <form className="flex flex-col gap-y-3">
+          <Input className="border-0 border-b-[1px] rounded-none" 
+            name="Nome"
+            placeholder="Nome"/>
+          <Input className="border-0 border-b-[1px] rounded-none" 
+            name="Telefone"
+            placeholder="Telefone"/>
+        </form>
+      </form>
+      </div>
     </div>
   )
 }
